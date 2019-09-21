d05581dd7101e7263eaefc7478821d3a0ba6da12
Benoit Daloze <eregontp@gmail.com>
2019-09-21 20:09:39 +0200

    Add support for TruffleRuby

--- src/installer.ts
+++ src/installer.ts
@@ -1,15 +1,26 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as tc from '@actions/tool-cache';
+import * as os from 'os';
 import * as path from 'path';
 
 const IS_WINDOWS = process.platform === 'win32';
 
-export async function findRubyVersion(version: string) {
-  const installDir: string | null = tc.find('Ruby', version);
+export async function findRubyVersion(engineAndVersion: string) {
+  if (!engineAndVersion.includes('-')) {
+    engineAndVersion = 'Ruby-' + engineAndVersion;
+  }
+
+  const [engine, version] = engineAndVersion.split('-', 2);
+
+  let installDir: string | null = tc.find(engine, version);
+
+  if (!installDir && engine == 'truffleruby') {
+    installDir = await downloadTruffleRuby(version);
+  }
 
   if (!installDir) {
-    throw new Error(`Version ${version} not found`);
+    throw new Error(`Ruby version ${engine}-${version} not found`);
   }
 
   const toolPath: string = path.join(installDir, 'bin');
@@ -23,4 +34,26 @@ export async function findRubyVersion(version: string) {
   }
 
   core.addPath(toolPath);
+}
+
+async function downloadTruffleRuby(version: string): Promise<string> {
+  const arch = os.arch();
+  const platform = os.platform();
+  const convertedArch = arch == 'x64' ? 'amd64' : arch;
+  const convertedPlatform = platform == 'darwin' ? 'macos' : platform;
+
+  const fileName = `truffleruby-${version}-${convertedPlatform}-${convertedArch}`;
+  const baseUrl = 'https://github.com/oracle/truffleruby/releases/download';
+  const downloadUrl = `${baseUrl}/vm-${version}/${fileName}.tar.gz`;
+
+  let downloadPath = await tc.downloadTool(downloadUrl);
+  let extractedPath = await tc.extractTar(downloadPath);
+  // truffleruby extracts with a root folder that matches the fileName downloaded
+  let toolRoot = path.join(extractedPath, fileName);
+
+  // Run the post-install hook
+  await exec.exec(path.join(toolRoot, 'lib/truffle/post_install_hook.sh'));
+
+  // Install into the local tool cache
+  return await tc.cacheDir(toolRoot, 'truffleruby', version);
 }
