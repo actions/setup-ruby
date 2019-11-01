724667021bf41f7cfc303e935f4b1e81ab6b76dd
Masatoshi Iwasaki <mstshiwasaki@gmail.com>
2019-11-01 21:03:45 +0900

    Support ruby_version

--- src/setup-ruby.ts
+++ src/setup-ruby.ts
@@ -1,12 +1,30 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import {ExecOptions} from '@actions/exec/lib/interfaces';
 import {findRubyVersion} from './installer';
 
 async function run() {
   try {
     let version = core.getInput('version');
+
     if (!version) {
       version = core.getInput('ruby-version');
     }
+
+    if (!version) {
+      const options: ExecOptions = {};
+
+      // Ignore stderr because there is a fallback
+      // option to "ruby_version" parameter.
+      options.listeners = {
+        stdout: (data: Buffer) => {
+          version += data.toString();
+        }
+      };
+
+      await exec.exec('cat', ['.ruby-version'], options);
+    }
+
     await findRubyVersion(version);
   } catch (error) {
     core.setFailed(error.message);
