7fa2ee82f35e026aaaebce2064225223de7e4bd1
Sinoru <sinoru@me.com>
2019-11-23 17:14:20 +0900

    Auto lint

--- src/installer.ts
+++ src/installer.ts
@@ -2,16 +2,20 @@ import * as exec from '@actions/exec';
 
 const IS_MACOS = process.platform === 'darwin';
 
-export async function installXcode(version: string, appleID: string | null, appleIDPassword: string | null) {
+export async function installXcode(
+  version: string,
+  appleID: string | null,
+  appleIDPassword: string | null
+) {
   if (!IS_MACOS) {
     throw new Error(`${process.platform} is not supported!`);
   }
 
-  if (await exec.exec(`xcversion select ${version}`) != 0) {
+  if ((await exec.exec(`xcversion select ${version}`)) != 0) {
     if (!appleID) {
       throw new Error(`apple-id is required to download Xcode.`);
     }
-    
+
     if (!appleIDPassword) {
       throw new Error(`apple-id-password is required to download Xcode.`);
     }
@@ -21,8 +25,8 @@ export async function installXcode(version: string, appleID: string | null, appl
 
     resultCode = await exec.exec(`xcversion install ${version}`, undefined, {
       env: {
-        'XCODE_INSTALL_USER': appleID,
-        'XCODE_INSTALL_PASSWORD': appleIDPassword,
+        XCODE_INSTALL_USER: appleID,
+        XCODE_INSTALL_PASSWORD: appleIDPassword
       },
       listeners: {
         stdout: (data: Buffer) => {
@@ -40,7 +44,7 @@ export async function installXcode(version: string, appleID: string | null, appl
           output += data.toString();
         }
       }
-    })
+    });
     if (resultCode != 0) {
       throw `Failed to detect os with result code ${resultCode}. Output: ${output}`;
     }
@@ -51,7 +55,7 @@ export async function installXcode(version: string, appleID: string | null, appl
           output += data.toString();
         }
       }
-    })
+    });
     if (resultCode != 0) {
       throw `Failed to detect os with result code ${resultCode}. Output: ${output}`;
     }
